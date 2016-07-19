7fee432988f48a5192f421682ecff37e516a7291
YourtionGuo <yourtion@gmail.com>
2016-07-19 11:38:35 +0800

    Request Data Demo

---
+++ DemoAPIs/RequestDemo.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class RequestDemo extends Component {
+  render() {
+    return (
+      <View>
+        <Text
+          onPress={this._doXMLHttpRequest}
+          style={styles.btn}>
+          XMLHttpRequest请求数据
+        </Text>
+
+        <Text
+          onPress={this._doFetch}
+          style={styles.btn}>
+          Fetch请求数据
+        </Text>
+      </View>
+    )
+  }
+
+  _doXMLHttpRequest() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status === 200) {
+        alert('success' + request.responseText);
+      } else {
+        alert('error');
+      }
+    };
+
+    request.open('GET', 'https://www.baidu.com/');
+    request.send();
+  }
+
+  _doFetch() {
+    fetch('https://www.baidu.com/')
+      .then((data) => { return data.text(); })
+      .then(alert).catch(alert);
+  }
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
+    height: 35,
+    backgroundColor: '#3BC1FF',
+    color: '#FFF',
+    lineHeight: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- index.js
+++ index.js
@@ -10,14 +10,16 @@ import AsyncStorageDemo from './DemoAPIs/AsyncStorageDemo'
 import IOSAPIDemo from './DemoAPIs/IOSAPIDemo'
 import CommomAPIDemo from './DemoAPIs/CommomAPIDemo'
 import CameraRollDemo from './DemoAPIs/CameraRollDemo'
+import RequestDemo from './DemoAPIs/RequestDemo'
 
 export default class Index extends Component {
   render() {
     return (
       // <AsyncStorageDemo />
       // <IOSAPIDemo />
-      <CommomAPIDemo />
+      // <CommomAPIDemo />
       // <CameraRollDemo />
+      <RequestDemo />
     );
   }
 }
